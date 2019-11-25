370c3a5c2046f2fc605a43f65f9e6500009df6e8
Alexander A. Klimov <grandmaster@al2klimov.de>
2019-11-25 21:50:29 +0100

    package-factory: go generate ./...

--- package-factory/build.sh
+++ package-factory/build.sh
@@ -3,6 +3,11 @@ set -exo pipefail
 
 cd /src
 
-if [ -e go.mod ]; then go mod download; fi
+if [ -e go.mod ]; then
+	go mod download
+	go generate ./...
+fi
+
 .travis-ci/mk-pkgs.sh
+
 if [ -e go.mod ]; then go test -failfast -race -v -cover ./...; fi
